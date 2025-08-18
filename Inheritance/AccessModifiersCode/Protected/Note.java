14cd02019a1678b7766d328e5c77e7b558f4b61c
Arpita155 <arpitasahoo795@gmail.com>
2025-08-18 23:55:56 +0530

    Create Note.java

---
+++ Inheritance/AccessModifiersCode/Protected/Note.java
@@ -0,0 +1,10 @@
+package OOP.Inheritance.Access_ModifiersCode.Protected;
+
+public class Note {
+
+    // - Accessible within the same package
+    // - Accessible to subclasses even if they're in a different package
+    // - Commonly used for members that should be available to child classes but hidden from the general , Public
+
+
+}
