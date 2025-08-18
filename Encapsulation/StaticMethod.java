b5ab892ef68f2f6fb26678222085d158c20034ac
Arpita155 <arpitasahoo795@gmail.com>
2025-08-18 23:26:00 +0530

    Add files via upload

---
+++ Encapsulation/StaticMethod.java
@@ -0,0 +1,34 @@
+package OOP.Encapsulation;
+
+public class StaticMethod {
+    int m;
+    int n;
+    static int count = 0;
+
+    static {
+        System.out.println("This is static method class . I am static method block !");
+    }
+
+    StaticMethod(int m,int n){
+        this.m = m;
+        this.n = n;
+    }
+    static boolean checkNonZero(int n){    // helper function     // Static method can't access instance variable
+        if(n!=0){
+            return true;
+        }
+        return false;
+    }
+    public int performDivision(){
+        if(checkNonZero(this.n)){
+            return this.m/this.n;
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        StaticMethod chopShingara = new StaticMethod(10,3);
+        int result = chopShingara.performDivision();
+        System.out.println("This is main() of static method class.");
+        System.out.println(result);
+    }
+}
