e7993e7e99f70fd7d8410185d2de24728350cb39
Arpita155 <arpitasahoo795@gmail.com>
2025-08-18 23:54:46 +0530

    Create Animal.java

---
+++ Inheritance/AccessModifiersCode/Protected/Animal.java
@@ -0,0 +1,10 @@
+package OOP.Inheritance.Access_ModifiersCode.Protected;
+
+public class Animal {
+
+    protected void breathe(){
+        System.out.println("Breathing.....");
+        System.out.println("this method is in the Animal class");
+    }
+
+}
