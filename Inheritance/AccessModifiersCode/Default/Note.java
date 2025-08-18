4115a4226cbb0178d18be11b0a8be03d6f08725f
Arpita155 <arpitasahoo795@gmail.com>
2025-08-18 23:50:19 +0530

    Add files via upload

---
+++ Inheritance/AccessModifiersCode/Default/Note.java
@@ -0,0 +1,11 @@
+package OOP.Inheritance.Access_ModifiersCode.Default;
+
+public class Note {
+
+    // - Also called package private
+    // - Accessible only within the same package
+    // - Not accessible from different packages, even for subclasses
+    // - Useful for classes that work together within a package
+
+
+}
