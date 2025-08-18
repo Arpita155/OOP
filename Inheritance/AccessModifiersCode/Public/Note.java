42f400b7a424df356cacbd2c6c85c2c4350251cf
Arpita155 <arpitasahoo795@gmail.com>
2025-08-18 23:58:12 +0530

    Add files via upload

---
+++ Inheritance/AccessModifiersCode/Public/Note.java
@@ -0,0 +1,9 @@
+package OOP.Inheritance.Access_ModifiersCode.Public;
+
+public class Note {
+
+    // - Most permissive modifier
+    // - Accessible from any other class,regardless of package
+    // - Always inherited by subclasses
+    // - Best for methods and variables that need to be accessed widely
+}
