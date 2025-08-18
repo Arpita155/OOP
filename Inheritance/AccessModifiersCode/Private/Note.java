6c9bbae92702f35a49716586eb8dd6a0ecc722a5
Arpita155 <arpitasahoo795@gmail.com>
2025-08-18 23:52:45 +0530

    Add files via upload

---
+++ Inheritance/AccessModifiersCode/Private/Note.java
@@ -0,0 +1,9 @@
+package OOP.Inheritance.Access_ModifiersCode.Private;
+
+public class Note {
+
+    // - Most restrictive modifier
+    // - Accessible only within the declaring class
+    // - Not inherited by subclasses
+    // - Used for internal implementation details
+}
