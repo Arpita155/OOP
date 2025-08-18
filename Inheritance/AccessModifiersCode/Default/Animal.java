845ebb7ad4dbcece531a92c224d8fb850766b9f9
Arpita155 <arpitasahoo795@gmail.com>
2025-08-18 23:49:06 +0530

    Create Animal.java

---
+++ Inheritance/AccessModifiersCode/Default/Animal.java
@@ -0,0 +1,11 @@
+package OOP.Inheritance.Access_ModifiersCode.Default;
+
+import javax.crypto.spec.PSource;
+
+public class Animal {
+    void makeSound(){       // default access
+        System.out.println("start of Animal class");
+        System.out.println("Some Sound");
+        System.out.println("End of Animal class");
+    }
+}
