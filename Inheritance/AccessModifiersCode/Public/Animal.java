0788d649b4771dd61202c85fb086fa7b631156a5
Arpita155 <arpitasahoo795@gmail.com>
2025-08-18 23:57:09 +0530

    Create Animal.java

---
+++ Inheritance/AccessModifiersCode/Public/Animal.java
@@ -0,0 +1,8 @@
+package OOP.Inheritance.Access_ModifiersCode.Public;
+
+public class Animal {
+    public void eat(){
+        System.out.println("Eating.......");
+        System.out.println("This eat method is in the Animal class");
+    }
+}
