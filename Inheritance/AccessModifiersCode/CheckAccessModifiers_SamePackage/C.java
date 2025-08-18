6ebf19db73b276f3f3ab4b4e962e57c8170ea8bc
Arpita155 <arpitasahoo795@gmail.com>
2025-08-18 23:46:59 +0530

    Add files via upload

---
+++ Inheritance/AccessModifiersCode/CheckAccessModifiers_SamePackage/C.java
@@ -0,0 +1,9 @@
+package OOP.Inheritance.Access_ModifiersCode.CheckAccessModifiers_SamePackage;
+
+class C {
+    int a;
+    C() {
+        this.a = 5;
+        System.out.println("This is a constructor with default access modifier within a default access modifier class.");
+    }
+}
