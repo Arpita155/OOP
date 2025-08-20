72f83ffeda1dc7d95860c1558de2905b5290a9fb
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-08-20 23:57:40 +0530

    Create Child.java

---
+++ Inheritance/Super_Final/UseOfFinal/FinalAppliedToMethod/Child.java
@@ -0,0 +1,10 @@
+package OOP.Inheritance.Super_Final.UseOfFinal.FinalAppliedToMethod;
+
+public class Child extends Parent {
+
+    // This would cause a compilation error
+//    void technology(){     // Can't override a final method
+//
+//    }
+
+}
