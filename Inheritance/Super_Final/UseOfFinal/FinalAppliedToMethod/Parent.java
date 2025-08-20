4c938f605e3a05b28e8eb4069969dedd190499d7
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-08-20 23:58:12 +0530

    Add files via upload

---
+++ Inheritance/Super_Final/UseOfFinal/FinalAppliedToMethod/Parent.java
@@ -0,0 +1,9 @@
+package OOP.Inheritance.Super_Final.UseOfFinal.FinalAppliedToMethod;
+
+public class Parent {
+
+    final void technology(){     // This method behavior can not be changed by child classes
+        System.out.println("Hello Students!");
+    }
+}
+
