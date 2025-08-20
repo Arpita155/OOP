c047560e0b41bd58376db18a1a74c579cbc1b9e4
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-08-21 00:07:51 +0530

    Add files via upload

---
+++ Inheritance/Super_Final/UseOfFinal/FinalAppliedToClass/ChildClass.java
@@ -0,0 +1,5 @@
+package OOP.Inheritance.Super_Final.UseOfFinal.FinalAppliedToClass;
+
+import OOP.Inheritance.Super_Final.UseOfSuper.Child;
+
+//class ChildClass extends ImmutableClass{}       // This would cause a compilation error. ----> because ImmutableClass class can not be inherited.
