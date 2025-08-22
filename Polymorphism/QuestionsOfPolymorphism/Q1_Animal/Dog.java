c2c0794e1e7a101bfb3e33cba149c7fe8304c22d
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-08-22 23:42:18 +0530

    Add files via upload

---
+++ Polymorphism/QuestionsOfPolymorphism/Q1_Animal/Dog.java
@@ -0,0 +1,9 @@
+package OOP.Polymorphism.QuestionsOfPolymorphism.Q1_Animal;
+
+public class Dog extends Animal{
+
+    public void makeSound(){
+        System.out.print("This is the makeSound method of Class Dog and ");
+        System.out.println("The Dog is barking..");
+    }
+}
