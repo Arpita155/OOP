1f958da623a62358659cd10695c3e4ddbd166252
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-08-24 00:44:52 +0530

    Add files via upload

---
+++ Abstraction/AbstractClass/ExampleOfAbstractClass/Ex3_GeneralConceptsOfEmployee/FullTimeEmployee.java
@@ -0,0 +1,15 @@
+package OOP.Abstraction.AbstractClass.ExampleOfAbstractClass.Ex4_GeneralConceptOfEmployee;
+
+public class FullTimeEmployee extends Employee{
+
+    private double monthlySalary;
+    public FullTimeEmployee(String name, double monthlySalary){
+        super(name);
+        this.monthlySalary = monthlySalary;
+    }
+    @Override
+    public double calculateSalary(){
+        return monthlySalary;
+    }
+
+}
