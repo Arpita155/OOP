33abcafad3cd7701bf959fb1445d69d0cf25038d
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-08-24 00:52:01 +0530

    Create A_DeclareAnAbstractClass.java

---
+++ Abstraction/AbstractClass/HowToUseAbstractClasses/A_DeclareAnAbstractClass.java
@@ -0,0 +1,10 @@
+package OOP.Abstraction.AbstractClass.HowToUseAbstractClasses;
+
+abstract class A_DeclareAnAbstractClass {
+
+    abstract void draw();    // Abstract method
+
+    void display(){     // concrete method
+        System.out.println("This is a shape");
+    }
+}
