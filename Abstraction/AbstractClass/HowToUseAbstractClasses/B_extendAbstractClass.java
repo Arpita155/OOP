fc39855782c2c44e1b605b936ffeb0fc48d7a30b
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-08-24 00:52:37 +0530

    Add files via upload

---
+++ Abstraction/AbstractClass/HowToUseAbstractClasses/B_extendAbstractClass.java
@@ -0,0 +1,9 @@
+package OOP.Abstraction.AbstractClass.HowToUseAbstractClasses;
+
+class B_extendAbstractClass extends A_DeclareAnAbstractClass{
+
+    void draw(){
+        System.out.println("Drawing a circle");
+    }
+
+}
