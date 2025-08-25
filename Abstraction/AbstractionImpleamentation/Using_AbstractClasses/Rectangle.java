f4baa074571b4ef5dc8b0546ae04641b92b0f8fa
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-08-25 23:32:19 +0530

    Add files via upload

---
+++ Abstraction/AbstractionImpleamentation/Using_AbstractClasses/Rectangle.java
@@ -0,0 +1,8 @@
+package OOP.Abstraction.AbstractionImplementation.Using_Abstract_Classes;
+
+class Rectangle extends Shape{
+
+    void draw(){
+        System.out.println("Drawing a Rectangle");
+    }
+}
