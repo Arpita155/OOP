68e7016ec0b67a8d372372102b03d9004703ab6d
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-08-25 23:34:55 +0530

    Add files via upload

---
+++ Abstraction/AbstractionImpleamentation/Using_Interfaces/Main.java
@@ -0,0 +1,14 @@
+package OOP.Abstraction.AbstractionImplementation.Using_Interfaces;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Animal dog = new Dog();
+        dog.eat();    // Dog eats bones
+        dog.sleep();    // Dog sleeps in kennel
+
+    }
+}
+
+
+
