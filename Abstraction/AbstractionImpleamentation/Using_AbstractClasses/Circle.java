b81d4b1150020a0fd999cb0915584c0ab2292269
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-08-25 23:30:55 +0530

    Create Circle.java

---
+++ Abstraction/AbstractionImpleamentation/Using_AbstractClasses/Circle.java
@@ -0,0 +1,8 @@
+package OOP.Abstraction.AbstractionImplementation.Using_Abstract_Classes;
+
+public class Circle extends Shape{
+
+    void draw(){
+        System.out.println("Drawing a circle");
+    }
+}
