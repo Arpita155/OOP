9f621bb16f342b27c81b971c0d62790597119ce7
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-08-27 00:47:28 +0530

    Add files via upload

---
+++ Abstraction/Interfaces/HowToUseInterface/ImplementTheInterface.java
@@ -0,0 +1,13 @@
+package OOP.Abstraction.Interfaces.HowToUseInterfaces;
+
+public class ImplementTheInterface implements A_defineTheInterface{
+    public void start(){
+        System.out.println("starting.....");
+    }
+    public void stop(){
+        System.out.println("Stopping...");
+    }
+    public void rot(){
+        System.out.println("rot");
+    }
+}
